32e10ad0e43d2fd604a46a2a74095500a484c312
JMEstrellanes <ictimus.jmestrellanes@gmail.com>
2026-03-31 13:38:18 +0800

    Enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 
     /**
